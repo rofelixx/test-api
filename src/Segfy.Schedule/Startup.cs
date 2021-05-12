b53c2145d6f18e6f459b38f884881563f6f5caf9
rafael.monteiro <rafael.monteiro@segfy.com>
2021-05-12 14:20:18 -0300

    Removed consul.

--- src/Segfy.Schedule/Startup.cs
+++ src/Segfy.Schedule/Startup.cs
@@ -22,8 +22,7 @@ namespace Segfy.Schedule
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddConsulConfiguration(configuration);
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
             Configuration = builder.Build();
 
