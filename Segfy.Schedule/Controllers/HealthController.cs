3c7613eedc2270f0aa172b6b6ab383ce966664b1
Tiago Muller <tiago@Tiagos-MacBook-Pro.local>
2021-06-02 15:00:48 -0300

    Adding health controller

---
+++ Segfy.Schedule/Controllers/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Segfy.Schedule.Model.Filters;
+using Segfy.Schedule.Model.Dtos;
+using System.Threading.Tasks;
+using MediatR;
+using AutoWrapper.Wrappers;
+using Segfy.Schedule.Model.Schema;
+using System.Collections.Generic;
+using Segfy.Schedule.Infra.Mediators.ScheduleActions.Commands;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Segfy.Schedule.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class HealthController
+    {
+        public HealthController()
+        {
+        }
+
+        [HttpGet]
+        [Route("isalive")]
+        public IActionResult IsAlive()
+        {
+            return new OkResult();
+        }
+    }
+}
